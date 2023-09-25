d3b2d2215d58bbfa17cc6881e201b8c66a5df29f
upendra <uvsingh2336@gmail.com>
2023-09-25 12:19:11 +0530

    added error handing match in basic example

--- a1_basics/src/main.rs
+++ a1_basics/src/main.rs
@@ -3,25 +3,22 @@ use std::net::TcpListener;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    println!("1 - print ---- listener ------- {:?}",listener);
+    println!("1 - print ---- listener ------- {:?}", listener);
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        // let stream = stream.unwrap();
 
-        println!("2 - print ---- stream ------------ {:?}",stream);
-
-        println!("3 - print ---- TCP Connection established!");
-
-
-        // match stream {
-        //     Ok(stream) => {
-        //         // Handle the incoming connection in a separate thread or function.
-        //         // Here, you would typically read from and write to the `stream`.
-        //     }
-        //     Err(e) => {
-        //         // Handle errors, such as failed connections.
-        //         eprintln!("Error accepting connection: {}", e);
-        //     }
-        // }
+        match stream {
+            Ok(stream) => {
+                // Handle the incoming connection in a separate thread or function.
+                // Here, you would typically read from and write to the `stream`.
+                println!("2 - print ---- stream ------------ {:?}", stream);
+                println!("3 - print ---- TCP Connection established!");
+            }
+            Err(e) => {
+                // Handle errors, such as failed connections.
+                eprintln!("Error accepting connection: {}", e);
+            }
+        }
     }
 }
